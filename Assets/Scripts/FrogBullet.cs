48bce0c3b3c6f9805585bb625e177d3ac96014ac
yigithankuraner <yigithankuraner@ogr.eskisehir.edu.tr>
2025-12-27 05:47:14 +0300

    proje güncel

---
+++ Assets/Scripts/FrogBullet.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class FrogBullet : MonoBehaviour
+{
+    public float speed = 10f;
+    public float lifeTime = 3f;
+    public int damage = 1;
+
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().linearVelocity = transform.right * speed;
+        Destroy(gameObject, lifeTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        if (hitInfo.CompareTag("Enemy")) return;
+
+        if (hitInfo.CompareTag("Player"))
+        {
+        }
+
+        Destroy(gameObject);
+    }
+}
